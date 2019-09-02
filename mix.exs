6ed1256fa013eb42f2cc10b4cde71450690ce0f8
Robert Collins <beau@collins.pub>
2019-09-02 07:00:04 -0700

    Remove some PHX stuff

--- mix.exs
+++ mix.exs
@@ -49,11 +49,6 @@ defmodule Ingest.MixProject do
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     []
   end
