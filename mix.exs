bd673cdc8c73ff32969417e55280119ab80d6284
Robert Collins <beau@collins.pub>
2019-09-12 18:57:47 -0400

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ingest.MixProject do
   def project do
     [
       app: :ingest,
-      version: "0.2.0",
+      version: "0.2.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
