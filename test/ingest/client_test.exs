93dcfce110a511e16dcb6f2cde5f67d25f06e90e
Robert Collins <beau@collins.pub>
2019-09-18 09:00:56 -0700

    More test coverage

---
+++ test/ingest/client_test.exs
@@ -0,0 +1,13 @@
+defmodule Ingest.ClientTest do
+  use ExUnit.Case
+
+  test "get URL" do
+    response = URI.parse("http://example.blog")
+    |> Ingest.Client.get()
+
+    case response do
+      { :ok, response } ->
+        assert response.status_code == 200
+    end
+  end
+end
---
+++ test/ingest/date_time_test.exs
@@ -0,0 +1,15 @@
+defmodule Ingest.DateTimeTest do
+  use ExUnit.Case
+
+  test "fails to parse" do
+    assert_raise RuntimeError, fn ->
+      Ingest.DateTime.parse!("")
+    end
+  end
+
+  test "parses successfully" do
+    date = Ingest.DateTime.parse!("2018-02-05 12:30:00 UTC")
+
+    assert date == ~U[2018-02-05 12:30:00Z]
+  end
+end
