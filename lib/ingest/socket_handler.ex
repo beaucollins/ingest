6e13df490494dc4a26127b10919e7394560bed54
Robert Collins <beau@collins.pub>
2019-09-22 21:12:09 -0700

    Remove noise

--- lib/ingest/socket_handler.ex
+++ lib/ingest/socket_handler.ex
@@ -2,15 +2,10 @@ defmodule Ingest.SocketHandler do
   @behaviour :cowboy_websocket
 
   def init(request, state) do
-    IO.inspect(state, label: "Incoming websocket")
-    IO.inspect(self(), label: "Init <==")
     {:cowboy_websocket, request, state}
   end
 
   def websocket_init(state) do
-    IO.inspect(self(), label: "Websocket Processs")
-    IO.inspect(state)
-
     case :net_kernel.monitor_nodes(true, [{:node_type, :all}]) do
       :ok ->
         reply_nodes(state)
@@ -21,13 +16,10 @@ defmodule Ingest.SocketHandler do
   end
 
   def websocket_handle(frame, state) do
-    IO.inspect(frame, label: "Frame <==")
-    IO.inspect(self(), label: "websocket frame")
     {:ok, state}
   end
 
   def websocket_info(info, state) do
-    IO.inspect({self(), info}, label: "Info <==")
     case info do
       {:nodeup, _node, _type} ->
         reply_nodes(state)
@@ -37,13 +29,6 @@ defmodule Ingest.SocketHandler do
 
         _ -> {:ok, state}
     end
-  end
-
-  def terminate(state, handler_state, reason) do
-    IO.inspect(state)
-    IO.inspect(handler_state)
-    IO.inspect(reason, label: "terminated:")
-    IO.inspect(self(), label: "terminated PID:")
   end
 
   defp nodes do
